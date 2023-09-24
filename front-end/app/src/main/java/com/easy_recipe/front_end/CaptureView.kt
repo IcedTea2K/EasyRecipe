b51ff8458ec635c0ecdf7aa90c454c0803c01bc0
Minh Au <auduongminh2004@gmail.com>
2023-09-24 04:20:48 -0700

    save captured photos

--- front-end/app/src/main/java/com/easy_recipe/front_end/CaptureView.kt
+++ front-end/app/src/main/java/com/easy_recipe/front_end/CaptureView.kt
@@ -1,6 +1,7 @@
 package com.easy_recipe.front_end
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
@@ -17,7 +18,6 @@ import androidx.camera.core.ImageProxy
 import androidx.camera.view.LifecycleCameraController
 import androidx.camera.view.PreviewView
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -26,7 +26,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
@@ -34,8 +33,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
@@ -45,6 +42,12 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
+import java.io.File
+import java.io.FileOutputStream
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Objects
 
 class CaptureView : ComponentActivity() {
     private val requestPermissionLauncher = registerForActivityResult(
@@ -104,6 +107,8 @@ class CaptureView : ComponentActivity() {
                             buffer.get(bytes)
                             val bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.size, null)
                             image.close()
+                            val file = saveTempFile(bitmap)
+                            Log.i("Capture View", file.toString())
                         }
                     })
             }) },
@@ -147,5 +152,21 @@ class CaptureView : ComponentActivity() {
                 contentScale = ContentScale.None,
             )
         }
+    }
+
+    fun saveTempFile(bitmap: Bitmap) : File {
+        // Create an image file name
+        val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val imageFileName = "JPEG_" + timeStamp + "_"
+        val image = File.createTempFile(
+            imageFileName, /* prefix */
+            ".jpg", /* suffix */
+            externalCacheDir      /* directory */
+        )
+        val output = FileOutputStream(image)
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 85, output)
+        output.flush()
+        output.close()
+        return image
     }
 }
